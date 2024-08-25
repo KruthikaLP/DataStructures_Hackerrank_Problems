fa494bcbf71a1d388e298a35b73ea576c44a75ba
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-08-25 15:00:34 +0530

    Update 2D Array - DS.java

--- Arrays/2D Array - DS.java
+++ Arrays/2D Array - DS.java
@@ -21,9 +21,9 @@ class Result {
 
     public static int hourglassSum(List<List<Integer>> arr) {
     // Write your code here
-    int max=0;
+    int max=Integer.MIN_VALUE;
+    int sum=Integer.MIN_VALUE;
     for(int i=0;i<4;i++){
-    int sum=0;
         for(int j=0;j<4;j++){
             sum = arr.get(i).get(j)+arr.get(i).get(j+1)+arr.get(i).get(j+2)+
             arr.get(i+1).get(j+1)+arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+
